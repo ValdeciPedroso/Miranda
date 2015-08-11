21cb1fcb5ffabd57eaf76e074123c315c6932a7e
Andre Matos <andre_matos13@hotmail.com>
2015-08-11 10:35:38 -0300

    BD Atualizado

--- Miranda.sql
+++ Miranda.sql
@@ -1,23 +1,47 @@
 -- phpMyAdmin SQL Dump
--- version 3.2.4
+-- version 4.4.12
 -- http://www.phpmyadmin.net
 --
--- Servidor: localhost
--- Tempo de Geração: Ago 03, 2015 as 01:22 
--- Versão do Servidor: 5.1.41
--- Versão do PHP: 5.3.1
+-- Host: 127.0.0.1
+-- Generation Time: 11-Ago-2015 às 15:33
+-- Versão do servidor: 5.6.25
+-- PHP Version: 5.6.11
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Banco de Dados: `Miranda`
+-- Database: `miranda`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `album`
+--
+
+CREATE TABLE IF NOT EXISTS `album` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `legenda` varchar(255) NOT NULL,
+  `id_categoria` int(11) NOT NULL,
+  `id_imagens` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `album`
+--
+
+INSERT INTO `album` (`id`, `nome`, `legenda`, `id_categoria`, `id_imagens`) VALUES
+(1, 'Nome do album', 'Legenda do algum', 2, 1),
+(2, 'Casamento', 'Albúm de casamento', 1, 1),
+(3, 'álbum  novo', 'Aniversario', 2, 1);
 
 -- --------------------------------------------------------
 
@@ -26,21 +50,51 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 --
 
 CREATE TABLE IF NOT EXISTS `usuario` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `nome` varchar(30) CHARACTER SET utf8 NOT NULL,
   `telefone` varchar(30) CHARACTER SET utf8 NOT NULL,
   `email` varchar(20) CHARACTER SET utf8 NOT NULL,
-  `senha` longtext CHARACTER SET utf8 NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+  `senha` longtext CHARACTER SET utf8 NOT NULL
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `usuario`
 --
 
 INSERT INTO `usuario` (`id`, `nome`, `telefone`, `email`, `senha`) VALUES
-(1, 'Valdeci', '(41) 9766-9768', 'admin@admin', 'MTIzNA==');
+(1, 'Valdeci', '(41) 9766-9768', 'admin@admin', 'MTIzNA=='),
+(2, 'fdfdafdsf', 'dsfdsf', 'fdsfdsf@fdsf', 'ZHNmc2Rh');
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `album`
+--
+ALTER TABLE `album`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `album`
+--
+ALTER TABLE `album`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
+--
+-- AUTO_INCREMENT for table `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
