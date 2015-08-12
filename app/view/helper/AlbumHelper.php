04180745e95d40eeb9a26d9256b91086e0569996
Andre Matos <andre_matos13@hotmail.com>
2015-08-12 10:47:06 -0300

    Modificando classe AlbumHelper
    Adiciona o album, pega o id do album adicionado e adiciona as imagens
    referentes a esse album

--- app/view/helper/AlbumHelper.php
+++ app/view/helper/AlbumHelper.php
@@ -10,10 +10,38 @@
 				$nome = (isset($_POST['nome']))?($_POST['nome']):null;
 				$legenda = (isset($_POST['legenda']))?($_POST['legenda']):null;
 				$id_categoria = (isset($_POST['categoria']))?($_POST['categoria']):null;
-				$id_imagens = (isset($_POST['id_imagens']))?($_POST['id_imagens']):null;
+				$i = 0;// sera o contador de cada imagem do foreach
 
-				$cpc = new CadastroAlbumController(); 
-				echo $cpc->inserirAlbum();
+				$cpc = new CadastroAlbumController();//instanciando objeto CadastroAlbumController
+				$id_album = $cpc->inserirAlbum();// Insere album no banco e pega o id para inserir as imagens
+
+				$cadImg = new CadastroImagem();// instancia objeto CadastroImagem
+				//Cria um diretorio para salvar as imagens do album
+				if(!mkdir("assets/img/album-img/$nome", 0700)){
+					echo 'Atenção, Já existe um album com esse nome<br />';
+					return;
+				}
+				// percorre o array de imagens
+				foreach ($_FILES["imagens"]["error"] as $key => $error) {
+					// Definir o diretório onde salvar os arquivos.
+				    $destino = "assets/img/album-img/$nome/" . $_FILES["imagens"]["name"][$i];
+				   	
+				    // Move o arquivo para o diretório de destino
+				    move_uploaded_file($_FILES["imagens"]["tmp_name"][$i], $destino );
+				 	// echo "Imagem: ".$destino.'<br />';
+				    
+				    // Insere no BD cada imagem para o album adicionado
+				    $cadImg->id_album = $id_album;
+				    $cadImg->endereco = $_FILES["imagens"]["name"][$i];
+				    $cadImg->insertImagem();
+				    
+				    // Próximo arquivo a ser analisado
+				    $i++;
+				}
+				echo $i.' imagens adicionadas<br />';
+				
+				
+				
 					
 		}
  
