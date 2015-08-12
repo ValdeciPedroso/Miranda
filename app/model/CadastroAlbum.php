51abe58bf313ef4516a05b7dccc51cec145daa69
Andre Matos <andre_matos13@hotmail.com>
2015-08-12 10:44:06 -0300

    Removendo propriedades desnecessarias do CadastroAlbum

--- app/model/CadastroAlbum.php
+++ app/model/CadastroAlbum.php
@@ -9,21 +9,20 @@
 		public $nome;
 		public $legenda;
 		public $categoria;
-		public $imagens;
 		
 		public function insertAlbum(){
 		
 			$stmt = DBConn::getInstance()->prepare(
 						"INSERT INTO album
-							(nome,legenda,id_categoria,id_imagens) 
+							(nome,legenda,id_categoria) 
 							VALUES 
-							(:nome, :legenda,:categoria,:imagens)"
+							(:nome, :legenda,:categoria)"
 					);
 				
 			$arr[':nome'] = $this->nome;
 			$arr[':legenda'] = $this->legenda;
 			$arr[':categoria'] = $this->categoria;
-			$arr[':imagens'] = 1;// **** CORRIGIR **** (Ver como INSERE fotos no banco de dados)
+
 			
 			return ($stmt->execute($arr)) ? DBConn::getInstance()->lastInsertId() : false;
 		
