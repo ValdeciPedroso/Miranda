8823c6ff734fd63601cc106b8e3ce79075101e34
AndMorMat <andre_matos13@hotmail.com>
2015-08-12 23:43:05 -0300

    Alterando pagina inicio para exibir os ultimos 4 albuns cadastrados

--- app/view/inicio.php
+++ app/view/inicio.php
@@ -100,55 +100,10 @@
 	        <div class="container">
 	        	<div class="row">
 		            <div class="col-sm-12 work-title wow fadeIn">
-		                <h2>Our Latest Work</h2>
+		                <h2>Últimos Trabalhos</h2>
 		            </div>
 	            </div>
-	            <div class="row">
-	            	<div class="col-sm-3">
-		                <div class="work wow fadeInUp">
-		                    <img src="assets/img/portfolio/work1.jpg" alt="Lorem Website" data-at2x="assets/img/portfolio/work1.jpg">
-		                    <h3>Lorem Website</h3>
-		                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor...</p>
-		                    <div class="work-bottom">
-		                        <a class="big-link-2 view-work" href="assets/img/portfolio/work1.jpg"><i class="fa fa-search"></i></a>
-		                        <a class="big-link-2" href="portfolio.html"><i class="fa fa-link"></i></a>
-		                    </div>
-		                </div>
-	                </div>
-	                <div class="col-sm-3">
-		                <div class="work wow fadeInDown">
-		                    <img src="assets/img/portfolio/work2.jpg" alt="Ipsum Logo" data-at2x="assets/img/portfolio/work2.jpg">
-		                    <h3>Ipsum Logo</h3>
-		                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor...</p>
-		                    <div class="work-bottom">
-		                        <a class="big-link-2 view-work" href="assets/img/portfolio/work2.jpg"><i class="fa fa-search"></i></a>
-		                        <a class="big-link-2" href="portfolio.html"><i class="fa fa-link"></i></a>
-		                    </div>
-		                </div>
-		            </div>
-		            <div class="col-sm-3">
-		                <div class="work wow fadeInUp">
-		                    <img src="assets/img/portfolio/work3.jpg" alt="Dolor Prints" data-at2x="assets/img/portfolio/work3.jpg">
-		                    <h3>Dolor Prints</h3>
-		                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor...</p>
-		                    <div class="work-bottom">
-		                        <a class="big-link-2 view-work" href="assets/img/portfolio/work3.jpg"><i class="fa fa-search"></i></a>
-		                        <a class="big-link-2" href="portfolio.html"><i class="fa fa-link"></i></a>
-		                    </div>
-		                </div>
-		            </div>
-		            <div class="col-sm-3">
-		                <div class="work wow fadeInDown">
-		                    <img src="assets/img/portfolio/work4.jpg" alt="Sit Amet Website" data-at2x="assets/img/portfolio/work4.jpg">
-		                    <h3>Sit Amet Website</h3>
-		                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor...</p>
-		                    <div class="work-bottom">
-		                        <a class="big-link-2 view-work" href="assets/img/portfolio/work4.jpg"><i class="fa fa-search"></i></a>
-		                        <a class="big-link-2" href="portfolio.html"><i class="fa fa-link"></i></a>
-		                    </div>
-		                </div>
-		            </div>
-	            </div>
+	            <?php include("ultimosTrabalhos.php"); ?>
 	        </div>
         </div>
 
