e4aa9d18132321b5094b1c6cc1d3f92582b699ee
AndMorMat <andre_matos13@hotmail.com>
2015-08-16 22:15:10 -0300

    Exibindo na pagina inicial somente albuns com pelo menos uma imagem cadastrada

--- app/view/ultimosTrabalhos.php
+++ app/view/ultimosTrabalhos.php
@@ -9,22 +9,25 @@
         echo 'Nenhum trabalho cadastrado!';
     }
     foreach ($lista as $key => $value) { ?>
-		<div class="col-sm-3">
-	        <div class="work wow fadeInUp">
-	        	<?php 
-	        		$cadastroImagemController = new CadastroImagemController();
-	        		$imagens = $cadastroImagemController->getImagensAlbum($lista[$key]->id);
-	        	?>
-	            <?php echo '<img src="assets/img/album-img/'.$lista[$key]->id.'/'.$imagens[0]->endereco.'" alt="Lorem Website" data-at2x="assets/img/album-img/'.$lista[$key]->id.'/'.$imagens[0]->endereco.'">'; ?>
-	            <h3><?php echo $lista[$key]->nome; ?></h3>
-	            <p><?php echo $lista[$key]->legenda; ?></p>
-	            <div class="work-bottom">
-	                <?php echo '<a class="big-link-2 view-work" href="assets/img/album-img/'.$lista[$key]->id.'/'.$imagens[0]->endereco.'"><i class="fa fa-search"></i></a>'; ?>
-	                <a class="big-link-2" href="portfolio.html"><i class="fa fa-link"></i></a>
-	            </div>
-	        </div>
-	    </div>
-<?php } ?>
+            <?php 
+                $cadastroImagemController = new CadastroImagemController();
+                $imagens = $cadastroImagemController->getImagensAlbum($lista[$key]->id);
+            if(count($imagens) > 0){
+            ?>
+            		<div class="col-sm-3">
+            	        <div class="work wow fadeInUp">
+            	            <?php echo '<img src="assets/img/album-img/'.$lista[$key]->id.'/'.$imagens[0]->endereco.'" alt="Lorem Website" data-at2x="assets/img/album-img/'.$lista[$key]->id.'/'.$imagens[0]->endereco.'">'; ?>
+            	            <h3><?php echo $lista[$key]->nome; ?></h3>
+            	            <p><?php echo $lista[$key]->legenda; ?></p>
+            	            <div class="work-bottom">
+            	                <?php echo '<a class="big-link-2 view-work" href="assets/img/album-img/'.$lista[$key]->id.'/'.$imagens[0]->endereco.'"><i class="fa fa-search"></i></a>'; ?>
+            	                <a class="big-link-2" href="portfolio.html"><i class="fa fa-link"></i></a>
+            	            </div>
+            	        </div>
+            	    </div>
+<?php       } // fim if
+    }// fim foreach
+?>
 	
     <!-- <div class="col-sm-3">
         <div class="work wow fadeInDown">
