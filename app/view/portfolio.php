22c2e5dd85b3d6da900aa7ea132d8a63e873bf19
ValdeciPedroso <gemeos_valdeci@hotmail.com>
2015-08-24 21:06:17 -0300

    alteração nos ultimos trabalhos

---
+++ app/view/portfolio.php
@@ -0,0 +1,60 @@
+<?PHP echo GenericosHelper::getStyle(); ?>
+<?PHP echo GenericosHelper::getTopo("..::Home::.."); ?>
+
+        <!-- Slider -->
+        <div class="presentation-container">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-10 col-sm-offset-1">
+                        <div class="col-sm-12 wow fadeInLeftBig">
+	            		<h1><span class="violet">Nosso Portfólio</span>,  confira nossos trabalhos.</h1>
+	            		<p>Com excelência, bom gosto e principalmente qualidade para atender esta diversidade de clientes de modo especial e diferenciado, pois cada cliente é tratado com exclusividade, de um modo único e especial, nossos serviços são feitos desta forma para que o cliente possa sair satisfeito, com um material único e personalizado com o seu jeito e seu gosto.</p>
+	            	</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="work-container">
+	        <div class="container">
+	        	<div class="row">
+		            <div class="col-sm-12 work-title wow fadeIn">
+		                <h2>Albúns</h2>
+		            </div>
+	            </div>
+	            
+	        </div>
+        </div>
+        <!-- portifolio -->
+        <div class="portfolio-container">
+	        <div class="container">
+	            <div class="row">
+	            	<div class="col-sm-5 portfolio">
+	            	            <div class="col-sm-6"> 
+		                        <center><img  class="thumbnail foto"src="assets\img\album-img\55\11811439_637961026307365_391337495115376029_n.jpg" ></center>
+		                    	</div>
+		                    	<div class="col-sm-6 info "> 
+			                    	<h3>Nome do albúm</h3>
+			                    	<p>Detalhes sobre o albúm...</p>
+			                    	<a class="big-link-1" href="services.html">Veja Mais</a>
+			                    </div>
+					</div>
+					<div class="col-sm-offset-2 col-sm-5 portfolio">
+	            	           <div class="col-sm-6 info "> 
+			                    	<h3>Nome do albúm</h3>
+			                    	<p>Detalhes sobre o albúm...</p>
+			                    	<a class="big-link-1" href="services.html">Veja Mais</a>
+			                    </div>
+			                     <div class="col-sm-6"> 
+		                        <center><img  class="thumbnail  foto"src="assets\img\album-img\52\274093_Papel-de-Parede-Torre-Eiffel-Iluminada-Paris-Franca_1600x1200.jpg" ></center>
+		                    	</div>
+		                    	
+					</div>
+	            </div>
+	        </div>
+        </div>
+
+
+        <!-- Testimonials -->
+        <div class="testimonials-container">	
+        </div>
+ <?PHP echo GenericosHelper::getRodape();?>
