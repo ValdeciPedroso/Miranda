ec0105c1321deab42877d9fc6eaa588c1191fdaa
ValdeciPedroso <gemeos_valdeci@hotmail.com>
2015-09-11 21:26:36 -0300

    albuns e fotos

---
+++ app/view/album.php
@@ -0,0 +1,74 @@
+<?PHP echo GenericosHelper::getStyle(); ?>
+<?PHP echo GenericosHelper::getTopo("..::Home::.."); ?>
+
+        <!-- Slider -->
+        <div class="presentation-container">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-10 col-sm-offset-1">
+                        <div class="col-sm-5"> 
+		                    <center>
+		                    <img  class="albuns" alt="foto" src="assets\img\album-img\55\11811439_637961026307365_391337495115376029_n.jpg" >
+		                    </center>
+		                </div>   
+	            		<h1><span class="violet">Titulo do albúm</span></h1>
+                        
+	            		<p class="text-album"> Com excelência, bom gosto e principalmente qualidade para atender esta diversidade de clientes de modo especial e diferenciado, pois cada cliente é tratado com exclusividade, de um modo único e especial, nossos serviços são feitos desta forma para que o cliente possa sair satisfeito, com um material único e personalizado com o seu jeito e seu gosto. pois cada cliente é tratado com exclusividade, de um modo único e especial, nossos serviços são feitos desta forma para que o cliente possa sair satisfeito, com um material único e personalizado com o seu jeito e seu pois cada cliente é tratado com exclusividade, de um modo único e especial, nossos serviços são feitos desta forma para que o cliente possa sair satisfeito, com um material único e personalizado com o seu jeito e seu pois cada cliente é tratado com exclusividade, de um modo único e especial, nossos serviços são feitos desta forma para que o cliente possa sair satisfeito, com um material único e personalizado com o seu jeito e seu </p>
+	            		</div>
+	            	</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</div>
+<div class="container">
+	 <div class="row">
+		  <div class="col-sm-offset-1 col-sm-10 col-sm-offset-1 work-title wow fadeIn">
+		      <h2>Mais Fotos</h2>
+
+		  </div>
+     </div>
+     
+        <div class=" col-sm-offset-1 col-sm-10 ">
+         	<div class="col-sm-3">
+			        <div class="work wow fadeInUp ">
+			            <a class="thumbnail view-work" href="assets\img\album-img\55\11811439_637961026307365_391337495115376029_n.jpg">
+			            <div class="foto"><img class="ultimoTrabalhos" alt="foto" src="assets\img\album-img\55\11811439_637961026307365_391337495115376029_n.jpg"  data-at2x="assets\img\album-img\55\11811439_637961026307365_391337495115376029_n.jpg">
+			            </div><a>
+		                <h3>titulo</h3>
+			            <p>legenda</p>
+			      </div>
+             </div>
+             <div class="col-sm-3">
+			        <div class="work wow fadeInUp ">
+			            <a class="thumbnail view-work" href="assets\img\album-img\55\11811439_637961026307365_391337495115376029_n.jpg">
+			            <div class="foto"><img class="ultimoTrabalhos" alt="foto" src="assets\img\album-img\55\11811439_637961026307365_391337495115376029_n.jpg"  data-at2x="assets\img\album-img\55\11811439_637961026307365_391337495115376029_n.jpg">
+			            </div><a>
+		                <h3>titulo</h3>
+			            <p>legenda</p>
+			      </div>
+             </div>
+             <div class="col-sm-3">
+			        <div class="work wow fadeInUp ">
+			            <a class="thumbnail view-work" href="assets\img\album-img\55\11811439_637961026307365_391337495115376029_n.jpg">
+			            <div class="foto"><img class="ultimoTrabalhos" alt="foto" src="assets\img\album-img\55\11811439_637961026307365_391337495115376029_n.jpg"  data-at2x="assets\img\album-img\55\11811439_637961026307365_391337495115376029_n.jpg">
+			            </div><a>
+		                <h3>titulo</h3>
+			            <p>legenda</p>
+			      </div>
+             </div>
+             <div class="col-sm-3">
+			        <div class="work wow fadeInUp ">
+			            <a class="thumbnail view-work" href="assets\img\album-img\55\11811439_637961026307365_391337495115376029_n.jpg">
+			            <div class="foto"><img class="ultimoTrabalhos" alt="foto" src="assets\img\album-img\55\11811439_637961026307365_391337495115376029_n.jpg"  data-at2x="assets\img\album-img\55\11811439_637961026307365_391337495115376029_n.jpg">
+			            </div><a>
+		                <h3>titulo</h3>
+			            <p>legenda</p>
+			      </div>
+             </div>
+	     </div>
+  </div>
+        <!-- Testimonials -->
+        <div class="testimonials-container">	
+        </div>
+ <?PHP echo GenericosHelper::getRodape();?>
--- app/view/portfolio.php
+++ app/view/portfolio.php
@@ -25,32 +25,54 @@
 	        </div>
         </div>
         <!-- portifolio -->
+
+					 
         <div class="portfolio-container">
 	        <div class="container">
+	              <ul class="pagination pagination-centered">
+				    <li><a href="#"><< Anterior</a></li>
+				    <li><a href="#">1</a></li>
+				    <li><a href="#">2</a></li>
+				    <li><a href="#">3</a></li>
+				    <li><a href="#">4</a></li>
+				    <li><a href="#">Próximo >></a></li>
+				</ul>
 	            <div class="row">
-	            	<div class="col-sm-5 portfolio">
-	            	            <div class="col-sm-6"> 
-		                        <center><img  class="thumbnail foto"src="assets\img\album-img\55\11811439_637961026307365_391337495115376029_n.jpg" ></center>
-		                    	</div>
-		                    	<div class="col-sm-6 info "> 
-			                    	<h3>Nome do albúm</h3>
-			                    	<p>Detalhes sobre o albúm...</p>
-			                    	<a class="big-link-1" href="services.html">Veja Mais</a>
-			                    </div>
-					</div>
-					<div class="col-sm-offset-2 col-sm-5 portfolio">
-	            	           <div class="col-sm-6 info "> 
-			                    	<h3>Nome do albúm</h3>
-			                    	<p>Detalhes sobre o albúm...</p>
-			                    	<a class="big-link-1" href="services.html">Veja Mais</a>
-			                    </div>
-			                     <div class="col-sm-6"> 
-		                        <center><img  class="thumbnail  foto"src="assets\img\album-img\52\274093_Papel-de-Parede-Torre-Eiffel-Iluminada-Paris-Franca_1600x1200.jpg" ></center>
-		                    	</div>
-		                    	
-					</div>
-	            </div>
-	        </div>
+	            	<div class="col-sm-offset-1 col-sm-10 portfolio">
+	                    <div class="col-sm-6"> 
+		                    <center><img  class="albuns" alt="foto" src="assets\img\album-img\55\11811439_637961026307365_391337495115376029_n.jpg" ></center>
+		                </div>
+		               	<div class="col-sm-6 info work-bottom descricao_album "> 
+			          	  <h3>Nome do albúm</h3>
+		               	  <p>Detalhes sobre o albúm.. of iteratura, contos, crônicas, romances, poesia, história em quadrinhos, infantil e infantojuvenil.Não serão aceitos livros didáticos, técnicos, religiosos, jornais e revistas de assuntos gerais ou de notícias, manuais, guias, enciclopédias, bem como qualquer material com teor ofensivo, discriminatório e pornográfico.
+
+Grandes doadores, interessados em doar acima de 1.000 volumes, precisarão se adequar a Edital de Chamamento Público lançado no dia 1º de abril de 2013. sasdasd asd as dasd .</p>
+		               	  <a class="big-link-1" href="album">Veja Mais</a>
+			            </div>
+			         </div>
+			         <div class="col-sm-offset-1 col-sm-10 portfolio">
+		               	<div class="col-sm-6 info work-bottom descricao_album "> 
+			          	  <h3>Nome do albúm</h3>
+		               	  <p>Detalhes sobre o albúm.asdfasdfasdfasdfasdfadsfa É Ferro na Boneca!, álbum de estreia do Novos Baianos ...
+rollingstone.uol.com.br/.../e-ferro-na-boneca-album-de-estreia-do-novos...
+16 de ago de 2015 - O álbum de estreia do Novos Baianos será relançado pela Polysom, como parte da coleção Clássicos em Vinil. Lançado originalmente entre ...sdasdasdf dsaf as df asdf asd fas df asdf asd fa sdf asdf asd fasd f..</p>
+		               	  <a class="big-link-1" href="album">Veja Mais</a>
+			            </div>
+			            <div class="col-sm-6"> 
+		                    <center><img  class="albuns" alt="foto" src="assets\img\album-img\52\wallpaper-da-selva.jpg" ></center>
+		                </div>
+			         </div>
+			    </div>
+			    <ul class="pagination pagination-centered">
+				    <li><a href="#"> << Anterior </a></li>
+				    <li><a href="#">1</a></li>
+				    <li><a href="#">2</a></li>
+				    <li><a href="#">3</a></li>
+				    <li><a href="#">4</a></li>
+				    <li><a href="#"> Próximo >></a></li>
+				</ul>
+	         </div>
+	    </div>
         </div>
 
 
