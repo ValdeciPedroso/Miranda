ac8670d996df7d2ab2ba8e597fd0063b58507562
ValdeciPedroso <gemeos_valdeci@hotmail.com>
2015-09-29 23:01:04 -0300

    ajustes na tel inicial
    Adicionado os apoios

--- app/view/ultimosTrabalhos.php
+++ app/view/ultimosTrabalhos.php
@@ -11,23 +11,23 @@ if(count($lista) != 0){
 			                <h2>Albúns em destaque</h2>
 			            </div>
 	</div>
-	<div class="row">
+	<div class="row ">
 
 	<?php
     foreach ($lista as $key => $value) { ?>
 
-		<div class="col-sm-3">
+		<div class="col-sm-3 ">
 	        <div class="work wow fadeInUp ">
 	        	<?php 
 	        		$cadastroImagemController = new CadastroImagemController();
 	        		$imagens = $cadastroImagemController->getImagensAlbum($lista[$key]->id);
 	        	?>
 
-	            <?php echo '<a class="thumbnail view-work" href="assets/img/album-img/'.$lista[$key]->id.'/'.$lista[$key]->foto_principal.'"><div class="foto"><img class="ultimoTrabalhos" alt="foto" src="assets/img/album-img/'.$lista[$key]->id.'/'.$lista[$key]->foto_principal.'"  data-at2x="assets/img/album-img/'.$lista[$key]->id.'/'.$lista[$key]->foto_principal.'"></div><a>'; ?>
-	            
+	            <?php echo '<a class="thumbnail trabalhosDestaque view-work" href="assets/img/album-img/'.$lista[$key]->id.'/'.$lista[$key]->foto_principal.'"><div class="foto"><img class="ultimoTrabalhos" alt="foto" src="assets/img/album-img/'.$lista[$key]->id.'/'.$lista[$key]->foto_principal.'"  data-at2x="assets/img/album-img/'.$lista[$key]->id.'/'.$lista[$key]->foto_principal.'"></div><a>'; ?>
+	            <div class="ultimosTrabalhos">
                 <h3><?php echo $lista[$key]->nome; ?></h3>
 	            <p><?php echo $lista[$key]->legenda; ?></p>
-	            
+	            </div>
                 <div class="work-bottom">
 	                <?php echo '<a class="big-link-2 view-work" href="assets/img/album-img/'.$lista[$key]->id.'/'.$lista[$key]->foto_principal.'"><i class="fa fa-search"></i></a>'; ?>
 	                <a class="big-link-2" href="portfolio.html"><i class="fa fa-link"></i></a>
